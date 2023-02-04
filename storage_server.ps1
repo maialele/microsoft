c6c81c768238aab8e0d35d3328516b7f2fb96c7f
maialele <75091525+maialele@users.noreply.github.com>
2023-02-04 18:32:40 +0200

    Update storage_server.ps1

--- storage_server.ps1
+++ storage_server.ps1
@@ -4,4 +4,4 @@ New-AzResourceGroup -Name $rg -Location northeurope -Force
 New-AzResourceGroupDeployment `
     -Name 'new-storage' `
     -ResourceGroupName $rg `
-    -TemplateFile '01-storage.json' 
+    -TemplateFile 'storageTemplate.json' 
