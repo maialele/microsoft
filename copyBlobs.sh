316ba3e8f2f0e1c414d657900c0f8f198280ca16
maialele <75091525+maialele@users.noreply.github.com>
2023-02-05 14:57:34 +0200

    Create copyBlobs.sh

---
+++ copyBlobs.sh
@@ -0,0 +1,1 @@
+azcopy copy 'https://<source-storage-account-name>.blob.core.windows.net/<container-name>/<blob-path>' 'https://<destination-storage-account-name>.blob.core.windows.net/<container-name>/<blob-path>'
