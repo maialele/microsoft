380043e51cee2631c6a95c482536e9998774f86d
maialele <75091525+maialele@users.noreply.github.com>
2023-02-06 18:35:14 +0200

    Update deployStorage_Server.ps1

--- deployStorage_Server.ps1
+++ deployStorage_Server.ps1
@@ -2,6 +2,12 @@ $rg = 'mayaResourceGroup'
 New-AzResourceGroup -Name $rg -Location northeurope -Force
 
 New-AzResourceGroupDeployment `
-    -Name 'storage-linux' `
+    -Name 'storage' `
     -ResourceGroupName $rg `
     -TemplateFile 'storageTemplate.json' 
+    
+New-AzResourceGroupDeployment `
+    -Name 'linux servier' `
+    -ResourceGroupName $rg `
+    -TemplateFile 'linuxTemplate.json' 
+
