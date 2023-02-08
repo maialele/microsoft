adf5715a3269c5557047a5fdd700932e786d6c62
maialele <75091525+maialele@users.noreply.github.com>
2023-02-08 21:45:24 +0200

    Update deployStorageAndServer.ps1

--- deployStorageAndServer.ps1
+++ deployStorageAndServer.ps1
@@ -7,13 +7,11 @@ $templateFile = "storageTemplate.json"
 # Deploy storage
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFile
 
-$templateFile = "windowsTemplate.json"
 # Deploy windows server
 $adminUsername = "maya"
 $adminPassword = $SERVER_PASSWORD
 $dnsLabelPrefix = "mayawinserver01"
 
-New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -TemplateUri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/quickstarts/microsoft.compute/vm-simple-windows/azuredeploy.json" `
